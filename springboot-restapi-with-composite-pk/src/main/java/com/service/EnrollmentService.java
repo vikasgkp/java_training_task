c0935cd54b490396d0ac5c8ec6e1033e7dd906b2
vikasgkp <160530320+vikasgkp@users.noreply.github.com>
2024-08-29 22:53:17 +0530

    Update EnrollmentService.java

--- springboot-restapi-with-composite-pk/src/main/java/com/service/EnrollmentService.java
+++ springboot-restapi-with-composite-pk/src/main/java/com/service/EnrollmentService.java
@@ -21,52 +21,20 @@ public class EnrollmentService {
 	}
 	
 	
-//	@Transactional
-//	public String deleteStudentByAge(int age) {
-//		if(studentRepository.deleteStudentById(age)>0) {
-//			return "Student record deleted";
-//		}else {
-//			return "Student record not present";
-//		}
-//
-//	}
+
 	
 	public String EnrollStudent(Enrollment enrollment) {
 		
-//		Optional<Enrollment> result = enrollmentRepository.findById(enrollment.getSid());
-//		if(result.isPresent()) {
-//			return "SId must be unique";
-//		}else {
+		Optional<Enrollment> result = enrollmentRepository.findById(enrollment.getSid());
+		if(result.isPresent()) {
+			return "SId must be unique";
+		}else {
 			enrollmentRepository.save(enrollment);
 			return "Student Record stored successfuly";
 		}
 	}
 	
-//	public String EnrollStudent(int sid,int cid, int year) {
-//		if(enrollmentRepository.existsByStudentIdandCoursesId(sid, cid ))
-//		{
-//			return "SId and Cid must be unique";
-//		}
-//			
-//			Enrollment enrollment = new Enrollment();
-////			enrollment.setStudent(Students);
-////			enrollment.setCourse(courses);
-////			enrollment.setYear(year);
-//			enrollmentRepository.save(enrollment);
-//			return "Enroll Record stored successfuly";
-//		
-//	}
-//	
-//	
-//	
-////	public String deleteStudent(int sid) {
-////		Optional<Students> result = studentRepository.findById(sid);
-//		if(result.isPresent()) {
-//			studentRepository.deleteById(sid);
-//			return "Student record deleted successfully";
-//		}else {
-//			return "Student record not present";
-//		}
-//	}
+
+
 	
 //}
